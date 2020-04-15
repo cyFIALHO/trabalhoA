bc2cf21c550ee594c23e761d1081d63e49951aab
cyFIALHO <55353093+cyFIALHO@users.noreply.github.com>
2020-04-15 17:20:03 -0300

    Add files via upload

---
+++ JogadorDeFutsal.java
@@ -0,0 +1,30 @@
+package trabalho;
+
+public class JogadorDeFutsal extends Jogador {
+
+    @Override
+    public void treinarNaAcademia() {
+
+    }
+
+    @Override
+    public void repousar() {
+
+    }
+
+    @Override
+    public void treinarNoGinasio() {
+
+    }
+
+    @Override
+    public void prepararParaOJogo() {
+
+    }
+
+    @Override
+    public void Jogar() {
+
+    }
+
+}
---
+++ JogadorDeVolei.java
@@ -0,0 +1,30 @@
+package trabalho;
+
+public class JogadorDeVolei extends Jogador {
+
+    @Override
+    public void treinarNaAcademia() {
+
+    }
+
+    @Override
+    public void repousar() {
+
+    }
+
+    @Override
+    public void treinarNoGinasio() {
+
+    }
+
+    @Override
+    public void prepararParaOJogo() {
+
+    }
+
+    @Override
+    public void Jogar() {
+
+    }
+
+}
