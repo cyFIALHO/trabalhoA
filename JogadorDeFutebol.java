39f9048ff2ee832a21c5ce77d221185a95b70ec0
cyFIALHO <55353093+cyFIALHO@users.noreply.github.com>
2020-04-15 20:49:29 -0300

    Add files via upload

--- JogadorDeFutebol.java
+++ JogadorDeFutebol.java
@@ -4,27 +4,27 @@ public class JogadorDeFutebol extends Jogador {
 
     @Override
     public void treinarNaAcademia() {
-
+        System.out.println("Treino de fortalecimento muscular pela manhã");
     }
 
     @Override
     public void repousar() {
-
+        System.out.println("Repouso de duas horas pós treino de fortalecimento");
     }
 
     @Override
     public void treinarNoGinasio() {
-
+        System.out.println("Treinamento tático");
     }
 
     @Override
     public void prepararParaOJogo() {
-
+        System.out.println("Concentrar com a EQUIPE");
     }
 
     @Override
     public void Jogar() {
-
+        System.out.println("BORA PRO JOGO!!!");
     }
 
 }
